4e693a33f650f8e5ba47ae06e4baac42fca232d5
voidhyr <samdani295@gmail.com>
2026-05-17 00:17:08 +0530

    add 02_print_loop: print ESP32 RUNNING every second using puts

---
+++ embedded/02_print_loop/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    while (1)
+    {
+        puts("ESP32 RUNNING");
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
