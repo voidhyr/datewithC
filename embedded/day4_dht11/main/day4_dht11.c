323604b5473503bffec7a39d445e768c8ce0d100
voidhyr <samdani295@gmail.com>
2026-05-31 01:12:44 +0530

    dht11: add fail_count error handling for disconnected sensor

--- embedded/day4_dht11/main/day4_dht11.c
+++ embedded/day4_dht11/main/day4_dht11.c
@@ -12,17 +12,28 @@ static const char *TAG = "DHT";
 
 void app_main(void)
 {
-  float temperature;
-  float humidity;
+  float temperature, humidity;
   esp_err_t result;
+  
+  int fail_count = 0;
 
   while (1) {
     result = dht_read_float_data(DHT_TYPE_AM2301, DHT_PIN, &humidity, &temperature);
 
     if (result == ESP_OK) {
       ESP_LOGI(TAG, "Humididy: %.1f Temperature: %.1f", humidity, temperature);
+      fail_count = 0;
     } else {
       ESP_LOGE(TAG, "Failed to read DHT11");
+      fail_count += 1;
+      ESP_LOGE(TAG, "Fail count is: %d", fail_count);
+      if (fail_count >= 5) {
+        ESP_LOGW(TAG, "Sensor may be disconnected");
+    }
+    }
+
+    if (fail_count >= 5) {
+      ESP_LOGW(TAG, "Sensor may be disconnected");
     }
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
